2a575d982abb9caea84e919096f74ad714001c9e
khemraj <khemrajraj@73gmail.com>
2024-10-07 23:14:35 +0530

    w

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -178,19 +178,19 @@
 
     <!-- Footer -->
     <footer class="footer">
-        <a href="mailto:gurpreet@devgurpreet.com" class="footer__link btn">Contact Me</a>
+        <a href="mailto:khemrajraj73@gmail.com" class="footer__link btn">Contact Me</a>
         <ul class="social-list">
 
             <li class="social-list__item">
-                <a class="social-list__link" href="https://github.com/preetguraya">
+                <a class="social-list__link" href="https://github.com/khemrajmaloor">
                     <i class="fab fa-github"></i>
                     Github
                 </a>
-                <a class="social-list__link" href="https://discord.com/users/Gurpreet#4001">
+                <a class="social-list__link" href="https://discord.com/users/Khemraj#4001">
                     <i class="fab fa-discord"></i>
                     Discord
                 </a>
-                <a class="social-list__link" href="https://www.linkedin.com/in/dev-gurpreet/">
+                <a class="social-list__link" href="https://www.linkedin.com/in/khem-raj/">
                     <i class="fab fa-linkedin"></i>
                     Linkedin
                 </a>
