fd4a3186b901b8433c1cb5e4b8cdf3ca2f840d53
Khemraj <khemraj@rwit.io>
2024-10-08 15:39:23 +0530

    change my work section

--- resources/views/partials/work.blade.php
+++ resources/views/partials/work.blade.php
@@ -1,53 +1,51 @@
-<h2 class="section__title section__title--work">My work</h2>
+{{-- <h2 class="section__title section__title--work">My work</h2> --}}
+<h2 class="section__title section__title--work">MDSF ASFASDF</h2>
 <p class="section__subtitle section__subtitle--work">A selection of my range of work</p>
 
 <div class="portfolio">
     <!-- Portfolio item 01 -->
-    <a href="portfolio-item1" class="portfolio__item" onmouseover="showCaption(this)" onmouseout="hideCaption(this)">
+    {{-- <a href="portfolio-item1" class="portfolio__item" onmouseover="showCaption(this)" onmouseout="hideCaption(this)">
         <div class="portfolio-item-caption">Ecommerce Site</div>
         <img src="img/book2.png" alt="" class="portfolio__img">
-    </a>
+    </a> --}}
 
     <!-- Portfolio item 02 -->
-    <a href="portfolio-item2.html" class="portfolio__item" onmouseover="showCaption(this)"
+    {{-- <a href="portfolio-item2.html" class="portfolio__item" onmouseover="showCaption(this)"
         onmouseout="hideCaption(this)">
         <div class="portfolio-item-caption">Backend Panel</div>
         <img src="img/admin1.png" alt="" class="portfolio__img">
-    </a>
+    </a> --}}
 
     <!-- Portfolio item 03 -->
-    <a href="portfolio-item3.html" class="portfolio__item" onmouseover="showCaption(this)"
+    {{-- <a href="portfolio-item3.html" class="portfolio__item" onmouseover="showCaption(this)"
         onmouseout="hideCaption(this)">
         <div class="portfolio-item-caption">React Game</div>
         <img src="img/tenzies.png" alt="" class="portfolio__img">
-    </a>
+    </a> --}}
 
     <!-- Portfolio item 04 -->
-    <a href="portfolio-item4.html" class="portfolio__item" onmouseover="showCaption(this)"
+    {{-- <a href="portfolio-item4.html" class="portfolio__item" onmouseover="showCaption(this)"
         onmouseout="hideCaption(this)">
         <div class="portfolio-item-caption">Memes App</div>
         <img src="img/meme.png" alt="" class="portfolio__img">
-    </a>
-    <!--
-      Portfolio item 05
-      <a href="portfolio-item.html" class="portfolio__item">
-          <img src="img/tenzies.png" alt="" class="portfolio__img">
-      </a>
+    </a> --}}
 
-      Portfolio item 06
-      <a href="portfolio-item.html" class="portfolio__item">
-          <img src="img/tenzies.png" alt="" class="portfolio__img">
-      </a>
 
-      Portfolio item 07
-      <a href="portfolio-item.html" class="portfolio__item">
-          <img src="img/tenzies.png" alt="" class="portfolio__img">
-      </a>
-
-      Portfolio item 08
-      <a href="#" class="portfolio__item">
-          <img src="img/tenzies.png" alt="" class="portfolio__img">
-      </a> -->
+    <div class="portfolio__item devs">
+        <div class="skillset">  <div></div></div>
+        Lorem ipsum dolor sit amet. Nam repellat aliquam vel rerum repudiandae aut quae modi sed aperiam repellendus ex
+        officiis voluptatem sit sunt omnis et fuga animi.
+    </div>
+    <div class="portfolio__item devs">
+        <div class="skillset"> <div></div></div>
+        Lorem ipsum dolor sit amet. Nam repellat aliquam vel rerum repudiandae aut quae modi sed aperiam repellendus ex
+        officiis voluptatem sit sunt omnis et fuga animi.
+    </div>
+    <div class="portfolio__item devs">
+        <div class="skillset"> <div></div></div>
+        Lorem ipsum dolor sit amet. Nam repellat aliquam vel rerum repudiandae aut quae modi sed aperiam repellendus ex
+        officiis voluptatem sit sunt omnis et fuga animi.
+    </div>
 
 
 </div>
