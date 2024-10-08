917c4c7b5e24c6939869c01b3ad679f3a4eeaf39
khemraj <khemrajraj@73gmail.com>
2024-10-08 22:27:30 +0530

    Change content of work section

--- resources/views/partials/work.blade.php
+++ resources/views/partials/work.blade.php
@@ -1,44 +1,57 @@
-{{-- <h2 class="section__title section__title--work">My work</h2> --}}
+{{-- 
+<h2 class="section__title section__title--work">My work</h2>
+--}}
 <h2 class="section__title section__title--work">MDSF ASFASDF</h2>
 <p class="section__subtitle section__subtitle--work">A selection of my range of work</p>
-
 <div class="portfolio">
-    <!-- Portfolio item 01 -->
-    {{-- <a href="portfolio-item1" class="portfolio__item" onmouseover="showCaption(this)" onmouseout="hideCaption(this)">
-        <div class="portfolio-item-caption">Ecommerce Site</div>
-        <img src="img/book2.png" alt="" class="portfolio__img">
-    </a> --}}
-
-    <!-- Portfolio item 02 -->
-    {{-- <a href="portfolio-item2.html" class="portfolio__item" onmouseover="showCaption(this)"
-        onmouseout="hideCaption(this)">
-        <div class="portfolio-item-caption">Backend Panel</div>
-        <img src="img/admin1.png" alt="" class="portfolio__img">
-    </a> --}}
-
-    <!-- Portfolio item 03 -->
-    {{-- <a href="portfolio-item3.html" class="portfolio__item" onmouseover="showCaption(this)"
-        onmouseout="hideCaption(this)">
-        <div class="portfolio-item-caption">React Game</div>
-        <img src="img/tenzies.png" alt="" class="portfolio__img">
-    </a> --}}
-
-    <!-- Portfolio item 04 -->
-    {{-- <a href="portfolio-item4.html" class="portfolio__item" onmouseover="showCaption(this)"
-        onmouseout="hideCaption(this)">
-        <div class="portfolio-item-caption">Memes App</div>
-        <img src="img/meme.png" alt="" class="portfolio__img">
-    </a> --}}
-
-    <div class="portfolio__item devs">
-     <div class="pens-brush"><i class="fa-solid fa-paintbrush"></i></div>
-        Lorem ipsum dolor sit amet. Nam repellat aliquam vel rerum repudiandae aut quae modi sed aperiam repellendus ex
-        officiis voluptatem sit sunt omnis et fuga animi.
-    </div>
-    <div class="portfolio__item devs">
-        <div class="pens-brush"><i class="fa-solid fa-code"></i></div>
-        Lorem ipsum dolor sit amet. Nam repellat aliquam vel rerum repudiandae aut quae modi sed aperiam repellendus ex
-        officiis voluptatem sit sunt omnis et fuga animi.
-    </div>
-
+   <!-- Portfolio item 01 -->
+   {{-- 
+   <a href="portfolio-item1" class="portfolio__item" onmouseover="showCaption(this)" onmouseout="hideCaption(this)">
+      <div class="portfolio-item-caption">Ecommerce Site</div>
+      <img src="img/book2.png" alt="" class="portfolio__img">
+   </a>
+   --}}
+   <!-- Portfolio item 02 -->
+   {{-- 
+   <a href="portfolio-item2.html" class="portfolio__item" onmouseover="showCaption(this)"
+      onmouseout="hideCaption(this)">
+      <div class="portfolio-item-caption">Backend Panel</div>
+      <img src="img/admin1.png" alt="" class="portfolio__img">
+   </a>
+   --}}
+   <!-- Portfolio item 03 -->
+   {{-- 
+   <a href="portfolio-item3.html" class="portfolio__item" onmouseover="showCaption(this)"
+      onmouseout="hideCaption(this)">
+      <div class="portfolio-item-caption">React Game</div>
+      <img src="img/tenzies.png" alt="" class="portfolio__img">
+   </a>
+   --}}
+   <!-- Portfolio item 04 -->
+   {{-- 
+   <a href="portfolio-item4.html" class="portfolio__item" onmouseover="showCaption(this)"
+      onmouseout="hideCaption(this)">
+      <div class="portfolio-item-caption">Memes App</div>
+      <img src="img/meme.png" alt="" class="portfolio__img">
+   </a>
+   --}}
+   <div class="portfolio__item devs">
+      <div class="pens-brush" style="padding-bottom:7px;">
+         <i class="fa-solid fa-paintbrush" aria-hidden="true"></i>
+         <h3>Design</h3>
+      </div>
+      <p>I specialize in crafting visually stunning, user-friendly interfaces that seamlessly integrate with backend
+         systems. From concept to execution, I ensure pixel-perfect precision, responsive design, and optimal
+         performance across all devices.
+      </p>
+   </div>
+   <div class="portfolio__item devs">
+      <div class="pens-brush" style="padding-bottom:7px;">
+         <i class="fa-solid fa-code" aria-hidden="true"></i>
+         <h3>Development</h3>
+      </div>
+      <p>My development expertise encompasses database management and server-side logic, ensuring seamless
+         integration, robust security, and performance optimization for web applications.
+      </p>
+   </div>
 </div>
