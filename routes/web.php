d7c6184115cf71a8d81ea60a537568ad2342b6b1
Khemraj <khemraj@rwit.io>
2024-12-16 10:54:50 +0530

    Added comment in route for clearify

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+/**
+ * Web route URL 
+ * 
+ */
 Route::get('/', function () {
     return view('index');
 });
+
+// Book site page route
 Route::get('/portfolio-item1', function () {
     return view('book');
 });
